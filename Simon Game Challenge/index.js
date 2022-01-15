f772ca8c910b9051a9ca0e179fb56ed62aa49846
pranit <patilpranit3112@gmail.com>
2022-01-15 22:43:25 +0530

    DONE

--- Simon Game Challenge/index.js
+++ Simon Game Challenge/index.js
@@ -1,8 +1,9 @@
-// use strict
 
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern=[];
+
+var userClickedPattern=[];
 
 function nextSquence(){}{
 
@@ -14,9 +15,17 @@ function nextSquence(){}{
 
  $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 
-var song="sounds/" + randomChosenColour + ".mp3"
-var audio = new Audio(song);
-audio.play();
+ var song="sounds/" + randomChosenColour + ".mp3"; 
 
-
+ playSound(song);
 }
+
+function playSound(name) {
+    var audio = new Audio(name);
+    // audio.play();
+  }
+
+$(".btn").click(function() {
+    var userChosenColour=$(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+})
