4ebb171e22127a7ec7655701d28a2eef1941a5ef
pranit <patilpranit3112@gmail.com>
2022-02-22 00:05:27 +0530

    Complete 8 Challenges

--- Blog Website Boss-Level Challenge/ejs-challenge/app.js
+++ Blog Website Boss-Level Challenge/ejs-challenge/app.js
@@ -12,22 +12,45 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+
+
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+
 app.use(express.static("public"));
 
 
+app.get("/", function(req, res) {
+  res.render("home",{home:homeStartingContent});
+})
+
+app.get("/about", function(req, res) {
+  res.render("about",{about:aboutContent});
+})
+
+app.get("/contact", function(req, res) {
+  res.render("contact",{contact:contactContent});
+})
+
+app.get("/compose", function(req, res) {
+  res.render("compose");
+})
+
+app.post("/compose", function(req, res) {
+  const inputData=req.body.postTitle;
+  console.log(inputData);
+
+
+  res.redirect("/compose")
+
+})
 
 
 
 
 
 
-
-
-
-
-
-
-app.listen(3000, function() {
+app.listen(3000 || process.env.PORT, function() {
   console.log("Server started on port 3000");
 });
