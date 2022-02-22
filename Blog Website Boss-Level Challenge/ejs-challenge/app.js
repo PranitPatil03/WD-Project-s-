2c50d783e84a56bf9c3ae8c547a7790d6a7b99cc
pranit <patilpranit3112@gmail.com>
2022-02-22 23:57:31 +0530

    Challenge 21 & Project complete.

--- Blog Website Boss-Level Challenge/ejs-challenge/app.js
+++ Blog Website Boss-Level Challenge/ejs-challenge/app.js
@@ -1,8 +1,7 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _=require("lodash");
 
 const posts=[];
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -34,18 +33,16 @@ app.get("/compose", function(req, res) {
 })
 
 app.get("/posts/:topic", function(req, res) {
-  const topic=req.params.topic;
+
+  const topic=_.lowerCase(req.params.topic);
 
   posts.forEach(function(post){
 
-    const pTitle= post.title
+    const pTitle= _.lowerCase(post.title);
 
     if(pTitle===topic)
     {
-      console.log("Match Found!");
-    }else
-    {
-      console.log("No Match Found!");
+      res.render("post",{ptopic:post.title,pcontent:post.body});
     }
   })
 
@@ -61,12 +58,7 @@ const post={
 posts.push(post);
 
   res.redirect("/")
-
 })
-
-
-
-
 
 app.listen(3000 || process.env.PORT, function() {
   console.log("Server started on port 3000");
